83f8468744fab9a9717c26374e4c08940d62aff9
Hollin Wakefield <45081384+hollinwakefield@users.noreply.github.com>
2021-07-03 19:21:42 -0700

    Score formula template

---
+++ scoreFormula.js
@@ -0,0 +1,14 @@
+let currentScore = 0;
+let correctResponses = 0;
+let totalResponses = 0;
+
+let accuracy = correctResponses / totalResponses;
+
+let response;
+
+if (response === true) {
+  score += 10;
+}
+
+const accuracyBonus = accuracy * correctResponses * 10;
+const finalScore = currentScore + accuracyBonus;

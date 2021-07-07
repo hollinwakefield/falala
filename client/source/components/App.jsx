a766fd43bc3b627c5e1e74b2bdea31d61a4e9329
Hollin Wakefield <45081384+hollinwakefield@users.noreply.github.com>
2021-07-06 20:17:17 -0700

    Minor styling

--- client/source/components/App.jsx
+++ client/source/components/App.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 class App extends React.Component {
   render() {
-    return <h1>Hello there</h1>;
+    return (
+      <div className="body">
+        <h1>Putonghua</h1>
+        <h3>普通话</h3>
+      </div>
+    );
   }
 }
 

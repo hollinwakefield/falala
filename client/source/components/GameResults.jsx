34403bebf47dc9a1c54bbb7ed2f4a09244534f31
Hollin Wakefield <45081384+hollinwakefield@users.noreply.github.com>
2021-07-07 23:05:59 -0700

    Conditional rendering of username input

--- client/source/components/GameResults.jsx
+++ client/source/components/GameResults.jsx
@@ -8,23 +8,32 @@ const GameResults = (props) => {
     console.log(username);
   };
 
-  return (
-    <div className="game">
-      <h3>Your final score!</h3>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Enter your name to save your score:
-          <input
-            type="text"
-            required="required"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          ></input>
-        </label>
-        <button>Save Score</button>
-      </form>
-    </div>
-  );
+  if (props.username) {
+    return (
+      <div className="game">
+        <h3>Your final score!</h3>
+        Nice job, {props.username}!
+      </div>
+    );
+  } else {
+    return (
+      <div className="game">
+        <h3>Your final score!</h3>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Enter your name to save your score:
+            <input
+              type="text"
+              required="required"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+            ></input>
+          </label>
+          <button>Save Score</button>
+        </form>
+      </div>
+    );
+  }
 };
 
 export default GameResults;
